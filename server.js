be5832e3fee904f5e43487099afc208f713f6e63
kuruse <uuusgr@gmail.com>
2020-01-14 22:22:01 +0900

    second

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
     const express = require('express'),
           multer  = require('multer'),
           basicAuth = require('basic-auth-connect'),
+          fs = require('fs'),
           config = require('./conf/config.js'),
+          flist = require('./conf/flist.js'),
           upload = multer({ dest: 'uploads/' }),
           app = express();
     
@@ -11,6 +13,20 @@
 
     app.post('/', upload.single('data'), (req, res) => {
         console.log(req.file);
+        let destDir = './' + req.file.destination;
+        if (req.file.originalname.match(/\.(csv|json)$/) !== null) {
+            destDir = flist.base + '/' + flist.hpname + '/data/';
+        }
+        const strpath = './' + req.file.path;
+        const detstpath = destDir + req.file.originalname;
+        fs.copyFile(strpath, detstpath, (err) => {
+            if (err) throw err;
+            console.log('copy success');
+            fs.unlink(strpath, (err) => {
+                if (err) throw err;
+                console.log('delete success');
+            });
+        });
         res.end('success');
     });
 
